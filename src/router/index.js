f1e62ff7930319c0da4cc0323ffc1f3c876d1625
AbisaiSan <abisaiweb7@gmail.com>
2023-06-21 14:37:57 -0300

    ajustes front

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: () => import("../views/produtos/ProdutoIndex.vue"),
     },
     {
-      path: '/produtos',
-      name: 'ProdutoIndex',
-      component: () => import('../views/produtos/ProdutoIndex.vue')
+      path: "/produtos",
+      name: "ProdutoIndex",
+      component: () => import("../views/produtos/ProdutoIndex.vue"),
     },
     {
-      path: '/produtos/create',
-      name: 'ProdutoCreate',
-      component: () => import('../views/produtos/ProdutoCreate.vue')
+      path: "/produtos/create",
+      name: "ProdutoCreate",
+      component: () => import("../views/produtos/ProdutoCreate.vue"),
     },
     {
-      path: '/produtos/:id/edit',
-      name: 'ProdutoEdit',
-      component: () => import('../views/produtos/ProdutoEdit.vue'),
+      path: "/produtos/:id/edit",
+      name: "ProdutoEdit",
+      component: () => import("../views/produtos/ProdutoEdit.vue"),
       props: true,
-    }
-  ]
-})
+    },
+  ],
+});
 
-export default router
+export default router;
--- src/views/produtos/ProdutoCreate.vue
+++ src/views/produtos/ProdutoCreate.vue
@@ -1,49 +1,84 @@
 <script setup>
+import { reactive } from "vue";
 
-import { reactive } from 'vue';
-
-import useProdutos from '../../composables/produtos';
+import useProdutos from "../../composables/produtos";
 
 const { storeProdutos, errors } = useProdutos();
 
 const form = reactive({
-    nome: "",
-    descricao: "",
-    preco: ""
+  nome: "",
+  descricao: "",
+  preco: "",
 });
-
 </script>
 
 <template>
- <div class="mt-12"> 
-    <form class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md" @submit.prevent="storeProdutos(form)">
-        <div class="space-y-6">
-            <div class="mb-6">
-                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900">Nome</label>
-                <input type="text" id="nome" v-model="form.nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " required>
-                <div v-if="errors.nome">
-                    <span class="text-sm text-red-400">{{ errors.nome[0] }}</span>
-                </div>
-            </div>
-           
-            <div class="mb-6">
-                <label for="descricao" class="block mb-2 text-sm font-medium text-gray-900">Descrição</label>
-                <input type="text" id="descricao" v-model="form.descricao" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 " required>
-                <div v-if="errors.descricao">
-                    <span class="text-sm text-red-400">{{ errors.descricao[0] }}</span>
-                </div>
-            </div>
-            <div>
-                <label for="preco" class="block mb-2 text-sm font-medium text-gray-900 ">Preço</label>
-                <input type="tel" id="preco" v-model="form.preco" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="R$" required>
-                    <div v-if="errors.preco">
-                            <span class="text-sm text-red-400">{{ errors.preco[0] }}</span>
-                    </div>
-            </div>
+  <div class="mt-12 py-8">
+    <form
+      class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md"
+      @submit.prevent="storeProdutos(form)"
+    >
+      <div class="space-y-6">
+        <div class="mb-6">
+          <label for="nome" class="block mb-2 text-sm font-medium text-gray-900"
+            >Nome</label
+          >
+          <input
+            type="text"
+            id="nome"
+            v-model="form.nome"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+          <div v-if="errors.nome">
+            <span class="text-sm text-red-400">{{ errors.nome[0] }}</span>
+          </div>
         </div>
-        <div class="mt-4">
-            <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">Cadastrar</button>
+
+        <div class="mb-6">
+          <label
+            for="descricao"
+            class="block mb-2 text-sm font-medium text-gray-900"
+            >Descrição</label
+          >
+          <input
+            type="text"
+            id="descricao"
+            v-model="form.descricao"
+            class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+          <div v-if="errors.descricao">
+            <span class="text-sm text-red-400">{{ errors.descricao[0] }}</span>
+          </div>
         </div>
+        <div>
+          <label
+            for="preco"
+            class="block mb-2 text-sm font-medium text-gray-900"
+            >Preço</label
+          >
+          <input
+            type="tel"
+            id="preco"
+            v-model="form.preco"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="R$"
+            required
+          />
+          <div v-if="errors.preco">
+            <span class="text-sm text-red-400">{{ errors.preco[0] }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <button
+          type="submit"
+          class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded"
+        >
+          Cadastrar
+        </button>
+      </div>
     </form>
- </div>
+  </div>
 </template>
